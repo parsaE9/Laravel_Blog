22d507cedb2eca576565268a7486616ca46b5acd
Parsa Esfahani <parsaesfahani78@gmail.com>
2022-05-23 20:41:10 +0430

    solved image not showing problem by adding file extension

--- app/Services/repository_services.php
+++ app/Services/repository_services.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 
 function save_photo($image)
 {
     $image_name = $image->getClientOriginalName();
-    $image_hashed_name = hash('MD5', $image_name) . time();
+    $image_hashed_name = hash('MD5', $image_name) . time() . "." . Str::afterLast($image_name, '.');
     $image->storeAs('images', $image_hashed_name, 'public');
     return $image_hashed_name;
 }
